189d901f036db4b1932b013b4396f4f200810cc8
Amit Matani <amatani@ngmoco.com>
2010-11-10 16:33:05 -0800

    removed some items

--- src/com/miraphonic/androidmaptest/AndroidMapTest.java
+++ src/com/miraphonic/androidmaptest/AndroidMapTest.java
@@ -1,13 +1,24 @@
 package com.miraphonic.androidmaptest;
 
-import android.app.Activity;
 import android.os.Bundle;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
 
-public class AndroidMapTest extends Activity {
+public class AndroidMapTest extends MapActivity {
+	MapView mapView;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
+        
+    }
+    
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
     }
 }
